55a731e2b6b545259db20210a25d2857b3bd9d03
Idan <idan.hovav16@gmail.com>
2017-03-22 17:15:37 -0700

    Block postVideo suggestions too

--- blockSuggestions.js
+++ blockSuggestions.js
@@ -1,10 +1,18 @@
 // Written by Idan Hovav please don't be mean or bad.
+const removeNode = (node) => {
+	if (node && node.parentNode) {
+		node.parentNode.removeChild(node);
+	}
+}
 
 try {
-	var sidebar = document.getElementById("watch7-sidebar");
-	if (sidebar && sidebar.parentNode) {
-		sidebar.parentNode.removeChild(sidebar);
-	}
+		var sidebar = document.getElementById("watch7-sidebar");
+		removeNode(sidebar);
+		var postVideo = document.getElementsByClassName("ytp-endscreen-content");
+		var i;
+		for (i = 0; i < postVideo.length; i++) {
+			removeNode(postVideo[i]);
+		}
 } catch (err) {
 	console.log(err.message);
 }
